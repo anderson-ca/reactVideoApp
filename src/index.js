6113cccec7cede290604193c5cecd8534fc96452
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-01-31 14:09:48 -0600

    add constructor method to App, and refactore App to become a class based component

--- src/index.js
+++ src/index.js
@@ -16,27 +16,38 @@ const API_KEY = 'AIzaSyCoTqYxMyr_XKN5dCEuDtZV-WLLNsx7UpQ';
 * 2) a callback function that returns that data retrieved from
 *    the search.
 */
-YTsearch({key: API_KEY, term: 'muffins'}, function(data) {
-
-    console.log(data);
-});
 
 // create a new component. This component should produce some HTML.
 /*
 * A component is similar to a class. Because a component has to be
 * initiated in order to be used.
 */
-const App = () => {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {videos: []};
+
+
+        YTsearch({key: API_KEY, term: 'muffins'}, (videos) => {
+
+            this.setState({ videos });
+            // same as this.setState({ videos: videos });
+        });
+    }
 
     // JSX transpiles code to vanilla JS.
     /*
     * For multi-line JSX it is common to use parenthesis around the value being returned
     */
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
+    render() {
+
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
 };
 
 // take the component's generated HTMl and put is the web page (DOM).
